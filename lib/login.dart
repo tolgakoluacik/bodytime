a8bc764c52cfe078ecc9073c82ebfdb40d06c50a
Tolga <tolgakoluacik@outlook.com>
2020-09-22 23:53:19 +0300

    login page bug fixed

--- lib/login.dart
+++ lib/login.dart
@@ -85,197 +85,199 @@ class _LoginState extends State<Login> {
     return Container(
       color: Color(0xFF4E546C),
       child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network("https://www.bodytime01.com/assets/img/logo.png"),
-              SizedBox(
-                height: 24,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color(0x50FFFFFF),
-                      width: 2,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                child: Row(
-                  children: [
-                    Container(
-                      color: Color(0xFF5F657B),
-                      child: Icon(
-                        Icons.person,
-                        color: Colors.white,
-                        size: 32,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network("https://www.bodytime01.com/assets/img/logo.png"),
+                SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0x50FFFFFF),
+                        width: 2,
                       ),
-                      padding: EdgeInsets.all(10),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: TextField(
-                          controller: _textField,
-                          onChanged: (phone) {
-                            setState(() {
-                              _phone = phone.trim();
-                            });
-                          },
-                          style: TextStyle(
-                            color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  child: Row(
+                    children: [
+                      Container(
+                        color: Color(0xFF5F657B),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: TextField(
+                            controller: _textField,
+                            onChanged: (phone) {
+                              setState(() {
+                                _phone = phone.trim();
+                              });
+                            },
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintText: "Telefon Numarası",
+                                hintStyle: TextStyle(color: Colors.grey)),
                           ),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              hintText: "Telefon Numarası",
-                              hintStyle: TextStyle(color: Colors.grey)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0x50FFFFFF),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  child: Row(
+                    children: [
+                      Container(
+                        color: Color(0xFF5F657B),
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: TextField(
+                            controller: _textField2,
+                            onChanged: (password) {
+                              setState(() {
+                                _password = password.trim();
+                              });
+                            },
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintText: "Şifre",
+                                hintStyle: TextStyle(color: Colors.grey)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 2.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xFFFF6700),
+                            borderRadius: BorderRadius.circular(4)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 32.0, vertical: 16.0),
+                          child: Text(
+                            "Giriş Yap",
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
-                    )
+                      onPressed: () {
+                        // if (_onAuthorized != null) {
+                        //   _onAuthorized.call();
+                        // }
+                        LoginRequest(_phone, _password).call().then((result) {
+                          Storage.putString("token", result.token);
+                          Storage.putString("sessionUser", json.encode(result.subscriber));
+                          _onAuthorized();
+                        });
+                      },
+                    ),
+                    FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 2.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(4)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 36.0, vertical: 16.0),
+                          child: Text(
+                            "Kayıt Ol",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        _clearTextField();
+                        setState(() {
+                          _isLogin = false;
+                          _isRegister = true;
+                        });
+                      },
+                    ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color(0x50FFFFFF),
-                      width: 2,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                child: Row(
-                  children: [
-                    Container(
-                      color: Color(0xFF5F657B),
-                      child: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                        size: 32,
+                SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0x50FFFFFF),
+                        width: 2,
                       ),
-                      padding: EdgeInsets.all(10),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: TextField(
-                          controller: _textField2,
-                          onChanged: (password) {
-                            setState(() {
-                              _password = password.trim();
-                            });
-                          },
-                          style: TextStyle(
-                            color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: InkWell(
+                      child: Container(
+                        child: Text(
+                          'Şifrenizi mi unuttunuz?',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ForgotPassword(),
                           ),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              hintText: "Şifre",
-                              hintStyle: TextStyle(color: Colors.grey)),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  FlatButton(
-                    padding: EdgeInsets.symmetric(horizontal: 2.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xFFFF6700),
-                          borderRadius: BorderRadius.circular(4)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 32.0, vertical: 16.0),
-                        child: Text(
-                          "Giriş Yap",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                        );
+                      },
                     ),
-                    onPressed: () {
-                      // if (_onAuthorized != null) {
-                      //   _onAuthorized.call();
-                      // }
-                      LoginRequest(_phone, _password).call().then((result) {
-                        Storage.putString("token", result.token);
-                        Storage.putString("sessionUser", json.encode(result.subscriber));
-                        _onAuthorized();
-                      });
-                    },
-                  ),
-                  FlatButton(
-                    padding: EdgeInsets.symmetric(horizontal: 2.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(4)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 36.0, vertical: 16.0),
-                        child: Text(
-                          "Kayıt Ol",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    onPressed: () {
-                      _clearTextField();
-                      setState(() {
-                        _isLogin = false;
-                        _isRegister = true;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color(0x50FFFFFF),
-                      width: 2,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: InkWell(
-                    child: Container(
-                      child: Text(
-                        'Şifrenizi mi unuttunuz?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ForgotPassword(),
-                        ),
-                      );
-                    },
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
